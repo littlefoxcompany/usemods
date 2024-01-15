ab8f136dac4438dd61d6233f0a38905ccab44b8e
Jeremy Butler <jeremy@littlefox.studio>
2024-01-15 14:23:35 +1000

    Restructure files and folders

--- docs/docs.ts
+++ docs/docs.ts
@@ -8,7 +8,7 @@ const contentDirectory = resolve('./content/2.functions')
 
 const functionPattern = /\/\*\*[\s\S]*?\*\/\s*(export\s+function\s+[a-zA-Z0-9_]+\([^)]*\)\s*:\s*[a-zA-Z]+\s*(?:{[\s\S]*?})?)?/gms
 const metadataPattern = /\/\/\s+(title|description):\s+([^\r\n]*)/g
-const subtitlePattern = /\/\/ <subtitle>(.+?)\/\/ <\/subtitle>/gs
+// const subtitlePattern = /\/\/ <subtitle>(.+?)\/\/ <\/subtitle>/gs
 
 async function processFiles() {
   try {
@@ -33,37 +33,33 @@ async function processFiles() {
 
 function generateMarkdown(tsContent: string): string {
   const metadata = Object.fromEntries([...tsContent.matchAll(metadataPattern)].map((m) => [m[1], m[2]]))
+
   let markdownContent = ''
 
   if (metadata.title) markdownContent += `# ${metadata.title}\n\n`
   if (metadata.description) markdownContent += `${metadata.description}\n\n`
 
-  const matches = [...tsContent.matchAll(functionPattern), ...tsContent.matchAll(subtitlePattern)]
+  const functions = tsContent.matchAll(functionPattern)
 
-  for (const match of matches) {
-    if (match[0].startsWith('// <subtitle>')) {
-      markdownContent += `## ${match[1].trim()}\n\n`
-    } else {
-      const name = (match[0].match(/export\s+function\s+([a-zA-Z0-9_]+)\s*\(/) || [])[1] || ''
+  for (const match of functions) {
+    const name = (match[0].match(/(?<=example )\w+/) || [])[0] || ''
+    const jsDoc = /\/\*\*([\s\S]*?)\*\//.exec(match[0])?.[1].trim() || ''
+    const description = jsDoc
+      .split('\n')
+      .map((line) => line.trim().replace(/\/?\*+/g, ''))
+      .slice(0, 1)
+      .join(' ')
+      .trim()
+    const example = (jsDoc.match(/@example\s+([^\r\n]*)/) || [])[1] || ''
+    const returns = (jsDoc.match(/@returns?\s+([^\r\n]*)/) || [])[1] || ''
 
-      const jsDoc = /\/\*\*([\s\S]*?)\*\//.exec(match[0])?.[1].trim() || ''
-      const description = jsDoc
-        .split('\n')
-        .map((line) => line.trim().replace(/\/?\*+/g, ''))
-        .slice(0, 1)
-        .join(' ')
-        .trim()
-      const example = (jsDoc.match(/@example\s+([^\r\n]*)/) || [])[1] || ''
-      const returns = (jsDoc.match(/@returns?\s+([^\r\n]*)/) || [])[1] || ''
-
-      if (name) markdownContent += `### ${name}\n\n`
-      if (description) markdownContent += `${description}\n\n`
-      if (example) markdownContent += '```js [js]\n' + example + '\n```\n\n'
-      if (returns) markdownContent += `**Returns:** ${returns}\n\n`
-    }
+    if (name) markdownContent += `### ${name}\n\n`
+    if (description) markdownContent += `${description}\n\n`
+    if (example) markdownContent += '```js [js]\n' + example + '\n```\n\n'
+    if (returns) markdownContent += `**Returns:** ${returns}\n\n`
   }
 
-  return markdownContent.replace(/\n\n\n/g, '\n\n')
+  return markdownContent
 }
 
 processFiles()
