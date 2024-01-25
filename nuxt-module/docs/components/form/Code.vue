2c19f07568fea38b3249f6593b136efd79bd0b95
Jeremy Butler <jeremy@littlefox.studio>
2024-01-26 08:19:29 +1000

    Add Code.vue component for form input

---
+++ nuxt-module/docs/components/form/Code.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="w-full">
+    <FormLabel :label="label" :info="info" />
+    <pre
+      id="notes"
+      contenteditable="true"
+      class="border w-full h-fit border-white/5 flex px-4 bg-white/[3%] rounded-lg focus:border-indigo-400 focus:ring-white focus:outline-0"
+      :placeholder="placeholder"
+      :value="modelValue"
+      @input="$event.target && $emit('update:modelValue', ($event.target as HTMLInputElement).value)">
+      {{ modelValue }}
+    </pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+    modelValue: {
+      type: [String, Number, Array, Object]
+    },
+    placeholder: {
+      type: String
+    },
+    label: {
+      type: String
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    info: {
+      type: String
+    }
+  })
+</script>
