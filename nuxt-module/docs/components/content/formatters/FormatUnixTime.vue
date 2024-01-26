8d1149929da138976862d6d036be606daf83c35e
Phillip Hartin <phillip@littlefox.studio>
2024-01-26 15:47:53 +1100

    Add format unix time component

---
+++ nuxt-module/docs/components/content/formatters/FormatUnixTime.vue
@@ -0,0 +1,14 @@
+<template>
+  <Example>
+    <ExampleInputs>
+      <FormInput label="Value" type="number" v-model.number="seconds" />
+    </ExampleInputs>
+    <ExampleResult>
+      {{ formatUnixTime(seconds) }}
+    </ExampleResult>
+  </Example>
+</template>
+
+<script setup lang="ts">
+  const seconds = ref(Math.round(Date.now() / 1000))
+</script>
