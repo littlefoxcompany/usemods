4cbb25799980d6d0ceaf640f256f571682e73529
Jeremy Butler <jeremy@littlefox.studio>
2024-01-29 19:14:25 +1000

    Add data functions and update validators

---
+++ src/7.data.ts
@@ -0,0 +1,191 @@
+// title: Data
+// description: A collection of functions for generating, manipulating and formatting arrays and objects.
+
+/**
+ * Shuffles an array.
+ */
+export function shuffle(items: any[]): any {
+  return items.sort(() => Math.random() - 0.5)
+}
+
+/**
+ * Returns unique array values with an optional property to pluck.
+ * @param items - The array of items.
+ * @param property - The property to pluck (optional).
+ */
+export function unique(property: string | null = null, ...arrays: (string | { [key: string]: any })[][]): any[] {
+  const combinedItems = arrays.flat()
+
+  if (!property) {
+    return [...new Set(combinedItems)]
+  }
+
+  const seenValues = new Set()
+  const result = []
+
+  for (const item of combinedItems) {
+    if (typeof item === 'object' && item[property] && !seenValues.has(item[property])) {
+      seenValues.add(item[property])
+      result.push(item)
+    }
+  }
+
+  return result
+}
+
+/**
+ * Returns the difference between two arrays.
+ */
+export function difference(...arrays: any[][]): any[] {
+  const mergedArray = arrays.flat()
+  return mergedArray.filter((item, index) => mergedArray.indexOf(item) === index)
+}
+
+/**
+ * Returns the first item in an array.
+ */
+export function first(items: any[]): string {
+  return items[0]
+}
+
+/**
+ * Returns the last item in an array.
+ */
+export function last(items: any[]): string {
+  return items[items.length - 1]
+}
+
+/**
+ * Returns the nth item in an array.
+ */
+export function nth(items: any[], nth: number): string {
+  return items[nth]
+}
+
+/**
+ * Offset the first item in an array.
+ */
+export function offset(items: any[], offset: number): any[] {
+  return items.slice(offset)
+}
+
+/**
+ * Groups an array of objects by a property.
+ */
+export function group(items: { [key: string]: any }[], property: string): { [key: string]: any } {
+  return items.reduce((accumulator, item) => {
+    const key = item[property]
+    if (!accumulator[key]) {
+      accumulator[key] = []
+    }
+    accumulator[key].push(item)
+    return accumulator
+  }, {})
+}
+
+/**
+ * Chunks an array into sections of a specified size.
+ */
+export function groupBy(items: any[], size: number): any[][] {
+  const result = []
+  for (let i = 0; i < items.length; i += size) {
+    result.push(items.slice(i, i + size))
+  }
+  return result
+}
+
+/**
+ * Flatten an array of arrays.
+ */
+export function flatten(items: any[]): any[] {
+  return items.reduce((accumulator, item) => {
+    if (Array.isArray(item)) {
+      return accumulator.concat(flatten(item)) // recursive call if item is an array
+    } else {
+      return accumulator.concat(item)
+    }
+  }, [])
+}
+
+/**
+ * Returns an array with a filtered out property.
+
+ */
+export function without(items: any[], properties: any | any[]): any[] {
+  if (!Array.isArray(items)) {
+    return items
+  }
+
+  if (Array.isArray(properties)) {
+    return items.filter((item) => !properties.includes(item))
+  } else {
+    return items.map((item) => {
+      if (typeof item === 'object' && item !== null) {
+        item = { ...item }
+        delete item[properties]
+      }
+      return item
+    })
+  }
+}
+
+/**
+ * Combine two or more arrays
+ */
+export function combineAll(...arrays: any[][]): any[] {
+  return ([] as any[]).concat(...arrays)
+}
+
+/**
+ * Combine two or more unique arrays
+ */
+export function combineUnique(...items: (any | any[])[]): any[] {
+  let combined: any[] = []
+
+  for (let item of items) {
+    if (Array.isArray(item)) {
+      combined = [...combined, ...item]
+    } else {
+      combined.push(item)
+    }
+  }
+
+  return Array.from(new Set(combined))
+}
+
+/**
+ * Combine two or more arrays or objects without a property.
+ */
+export function combineWithout(
+  property: string | number,
+  ...items: (({ [key: string]: any } | { [key: number]: any }) | ({ [key: string]: any } | { [key: number]: any })[])[]
+): any[] {
+  let combined: any[] = []
+
+  for (let item of items) {
+    if (Array.isArray(item)) {
+      combined = [...combined, ...item]
+    } else {
+      combined.push(item)
+    }
+  }
+
+  const unique = Array.from(new Set(combined.map((item) => item[property])))
+  return unique
+}
+
+/**
+ * Reverse an array.
+ */
+export function reverse(items: any[]): any[] {
+  return items.reverse()
+}
+
+/**
+ * Sort an array by a property.
+ */
+export function sortBy(items: { [key: string]: any }[], property: string): { [key: string]: any }[] {
+  return items.sort((a, b) => {
+    return a[property] > b[property] ? 1 : -1
+  })
+}
