fdb1a97170cc194efc054b73cab2b953513a52ab
Jeremy Butler <jeremy@littlefox.studio>
2024-02-01 13:57:34 +1000

    Add TableOfContents component and update default layout

---
+++ nuxt-module/docs/components/TableOfContents.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav>
+    <p class="font-semibold">On this page</p>
+    <NuxtLink v-for="link in links" :key="link" :to="'#' + link" class="py-1 flex font-medium text-gray-500 hover:text-white/75 gap-3" activeClass="active">
+      {{ link }}
+    </NuxtLink>
+  </nav>
+</template>
+
+<script setup lang="ts">
+  const route = useRoute()
+  const toc = await queryContent(route.fullPath).only('body').find()
+
+  const links = toc[0].body?.children?.map((link: any) => {
+    return link.props.name
+  })
+</script>
--- nuxt-module/docs/layouts/default.vue
+++ nuxt-module/docs/layouts/default.vue
@@ -5,7 +5,7 @@
 
     <!-- Pages -->
     <div class="container relative mx-auto flex min-h-screen py-12 gap-20">
-      <nav class="-mt-2 w-2/12 flex flex-col sticky h-fit top-4">
+      <nav class="-mt-2 w-2/12 flex flex-col sticky h-fit top-6">
         <div v-for="section in sidebar" class="pb-12 flex flex-col first:text-xl">
           <NuxtLink v-for="link in section.children" :key="link._path" :to="link._path" class="py-2 flex font-medium text-gray-500 hover:text-white/75 gap-3" activeClass="active">
             {{ link.title }}
@@ -14,22 +14,18 @@
       </nav>
 
       <!-- Content -->
-      <div class="min-h-screen w-8/12">
-        <NuxtPage class="w-full" />
+      <div class="min-h-screen w-7/12">
+        <ContentDoc class="w-full" />
       </div>
 
       <!-- Table of Contents -->
-      <nav class="shrink max-lg:hidden w-2/12 max-w-[150px] h-fit top-4 sticky">
-        {{ toc }}
-      </nav>
+      <TableOfContents class="shrink max-lg:hidden w-3/12 max-w-[150px] h-fit top-8 sticky" />
     </div>
   </main>
 </template>
 
 <script setup lang="ts">
-  const route = useRoute()
   const { data: sidebar } = await useAsyncData('sidebarLinks', () => fetchContentNavigation())
-  const { data: toc } = await useAsyncData('toc', () => queryContent().findOne())
 
   provide('docs', sidebar)
 </script>
