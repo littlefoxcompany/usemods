97ffe0f0fb8b17e182524c82cf019d5edb3d2784
Jeremy Butler <jeremy@littlefox.studio>
2024-02-08 10:43:03 +1000

    Add ToggleBodyScroll component

---
+++ nuxt-module/docs/components/content/actions/ToggleBodyScroll.vue
@@ -0,0 +1,13 @@
+<template>
+  <Example>
+    <ExampleInputs>
+      <Button color="secondary" class="w-full" @click="action">Toggle</Button>
+    </ExampleInputs>
+  </Example>
+</template>
+
+<script setup lang="ts">
+  function action() {
+    toggleBodyScroll()
+  }
+</script>
