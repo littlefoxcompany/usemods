556626db2c8efd3fac8f95cc465020e5fe5d3f84
Jeremy Butler <jeremy@littlefox.studio>
2024-02-20 16:15:08 +1000

    Refactor DropdownItem.vue styling

--- nuxt-module/docs/components/nav/DropdownItem.vue
+++ nuxt-module/docs/components/nav/DropdownItem.vue
@@ -1,5 +1,7 @@
 <template>
-  <NuxtLink :to="to" class="dark:text-white px-4 flex items-center gap-3 py-2.5 rounded-lg hover:bg-white/5 border border-transparent hover:border-white/10 transition-all">
+  <NuxtLink
+    :to="to"
+    class="dark:text-white text-gray-500 px-4 flex items-center gap-3 py-2.5 rounded-lg hover:bg-white/5 border border-transparent hover:border-white/10 transition-all">
     <slot />
   </NuxtLink>
 </template>
