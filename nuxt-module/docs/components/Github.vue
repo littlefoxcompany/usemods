2b7214f0b3aef6341489dd7bfcaae89fde574a0f
Jeremy Butler <jeremy@littlefox.studio>
2024-02-29 10:54:36 +1000

    Update page formatting

--- nuxt-module/docs/components/Github.vue
+++ nuxt-module/docs/components/Github.vue
@@ -2,7 +2,7 @@
   <div class="flex">
     <a href="https://github.com/LittleFoxCompany/usemods" class="border border-gray-200 dark:border-white/20 rounded-lg px-2 h-8 flex items-center gap-2">
       <Icon name="fa6-brands:github" class="w-4 h-4 dark:text-white" />
-      <span v-if="data" class="text-sm">
+      <span v-if="data?.stargazers_count" class="text-sm">
         {{ formatNumber(data?.stargazers_count, false) }}
       </span>
     </a>
