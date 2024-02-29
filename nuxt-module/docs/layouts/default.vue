940f9ea95c2816e1c01cd4149f3ecedb3e60cfa1
Phillip Hartin <phillip@littlefox.studio>
2024-02-29 13:22:47 +1100

    Prefetch doc links

--- nuxt-module/docs/layouts/default.vue
+++ nuxt-module/docs/layouts/default.vue
@@ -12,6 +12,7 @@
             :key="link._path"
             :to="link._path"
             class="py-2 flex font-medium text-gray-400 dark:text-gray-500 items-center dark:hover:text-white/75 gap-3"
+            :prefetch="true"
             active-class="active">
             <Icon :name="link.title" class="w-5 h-5 shrink-0" />
             {{ link.title }}
