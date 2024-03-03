f539efe3e9368f1e5c2c04d1c848a51371715412
Jeremy Butler <jeremy@littlefox.studio>
2024-03-04 09:14:43 +1000

    Remove unused functions and tests

--- test/modifiers.test.ts
+++ test/modifiers.test.ts
@@ -11,7 +11,6 @@ import {
   ordinalize,
   deslugify,
   slugify,
-  splitByWords,
   startWith,
   endWith,
   camelCase,
@@ -36,10 +35,6 @@ test('escapeHtml', () => {
 
 test('unescapeHtml', () => {
   expect(unescapeHtml('&lt;p&gt;Hello World&lt;/p&gt;')).toBe('<p>Hello World</p>')
-})
-
-test('stripTags', () => {
-  expect(stripTags('<p><strong>Hello</strong></p>')).toBe('Hello')
 })
 
 test('stripEmojis', () => {
@@ -90,23 +85,17 @@ test('ordinalize', () => {
   expect(ordinalize(104)).toBe('104th')
 })
 
-// test('splitByWords', () => {
-//   expect(splitByWords('Hello world. How are you?')).toBe(
-//     `<span class="sentence sentence-1"><span class="word word-1">Hello</span> <span class="word word-2">world.</span></span> <span class="sentence sentence-2"><span class="word word-3">How</span> <span class="word word-4">are</span> <span class="word word-5">you?</span></span>`
-//   )
+// test('title', () => {
+//   expect(title('A new welcome for my brand new test for titles in javascript!')).toBe('A New Welcome for My Brand New Test for Titles in Javascript!')
 // })
 
-test('title', () => {
-  expect(title('A new welcome for my brand new test for titles in javascript!')).toBe('A New Welcome for My Brand New Test for Titles in Javascript!')
-})
+// test('truncate', () => {
+//   expect(truncate('Hello world this is me', 4)).toBe('Hell...')
+// })
 
-test('truncate', () => {
-  expect(truncate('Hello world this is me', 4)).toBe('Hell...')
-})
-
-test('truncateWords', () => {
-  expect(truncateWords('Hello world and moon', 2)).toBe('Hello world...')
-})
+// test('truncateWords', () => {
+//   expect(truncateWords('Hello world and moon', 2)).toBe('Hello world...')
+// })
 
 // test('countWords', () => {
 //   expect(countWords('Hello world and moon')).toBe(4)
@@ -131,103 +120,83 @@ test('endWith', () => {
   expect(endWith('www.helloworld.com', '.com')).toBe('www.helloworld.com')
 })
 
-test('surround', () => {
-  expect(surround('helloworld', 'www.', '.com')).toBe('www.helloworld.com')
-  expect(surround('helloworld.com', 'www.', '.com')).toBe('www.helloworld.com')
-  expect(surround('www.helloworld', 'www.', '.com')).toBe('www.helloworld.com')
-  expect(surround('www.helloworld.com', 'www.', '.com')).toBe('www.helloworld.com')
+test('surroundWith', () => {
+  expect(surroundWith('helloworld', 'www.', '.com')).toBe('www.helloworld.com')
+  expect(surroundWith('helloworld.com', 'www.', '.com')).toBe('www.helloworld.com')
+  expect(surroundWith('www.helloworld', 'www.', '.com')).toBe('www.helloworld.com')
+  expect(surroundWith('www.helloworld.com', 'www.', '.com')).toBe('www.helloworld.com')
 })
 
-test('group', () => {
-  const items = [
-    { name: 'John', age: 25 },
-    { name: 'Jane', age: 30 },
-    { name: 'Jake', age: 25 }
-  ]
+// test('group', () => {
+//   const items = [
+//     { name: 'John', age: 25 },
+//     { name: 'Jane', age: 30 },
+//     { name: 'Jake', age: 25 }
+//   ]
 
-  expect(group(items, 'age')).toStrictEqual({
-    25: [
-      { name: 'John', age: 25 },
-      { name: 'Jake', age: 25 }
-    ],
-    30: [{ name: 'Jane', age: 30 }]
-  })
+//   expect(group(items, 'age')).toStrictEqual({
+//     25: [
+//       { name: 'John', age: 25 },
+//       { name: 'Jake', age: 25 }
+//     ],
+//     30: [{ name: 'Jane', age: 30 }]
+//   })
 
-  expect(group(items, 'name')).toStrictEqual({
-    John: [{ name: 'John', age: 25 }],
-    Jane: [{ name: 'Jane', age: 30 }],
-    Jake: [{ name: 'Jake', age: 25 }]
-  })
-})
+//   expect(group(items, 'name')).toStrictEqual({
+//     John: [{ name: 'John', age: 25 }],
+//     Jane: [{ name: 'Jane', age: 30 }],
+//     Jake: [{ name: 'Jake', age: 25 }]
+//   })
+// })
 
-test('groupBy', () => {
-  const chunk = ['a', 'b', 'c', 'd', 'e', 'f']
-  expect(groupBy(chunk, 2)).toStrictEqual([
-    ['a', 'b'],
-    ['c', 'd'],
-    ['e', 'f']
-  ])
-  expect(groupBy(chunk, 3)).toStrictEqual([
-    ['a', 'b', 'c'],
-    ['d', 'e', 'f']
-  ])
-  expect(groupBy(chunk, 4)).toStrictEqual([
-    ['a', 'b', 'c', 'd'],
-    ['e', 'f']
-  ])
-  expect(groupBy(chunk, 5)).toStrictEqual([['a', 'b', 'c', 'd', 'e'], ['f']])
-  expect(groupBy(chunk, 6)).toStrictEqual([chunk])
-})
+// test('groupBy', () => {
+//   const chunk = ['a', 'b', 'c', 'd', 'e', 'f']
+//   expect(groupBy(chunk, 2)).toStrictEqual([
+//     ['a', 'b'],
+//     ['c', 'd'],
+//     ['e', 'f']
+//   ])
+//   expect(groupBy(chunk, 3)).toStrictEqual([
+//     ['a', 'b', 'c'],
+//     ['d', 'e', 'f']
+//   ])
+//   expect(groupBy(chunk, 4)).toStrictEqual([
+//     ['a', 'b', 'c', 'd'],
+//     ['e', 'f']
+//   ])
+//   expect(groupBy(chunk, 5)).toStrictEqual([['a', 'b', 'c', 'd', 'e'], ['f']])
+//   expect(groupBy(chunk, 6)).toStrictEqual([chunk])
+// })
 
-test('flatten', () => {
-  expect(flatten([1, [2, [3, [4]], 5]])).toStrictEqual([1, 2, 3, 4, 5])
-})
+// test('flatten', () => {
+//   expect(flatten([1, [2, [3, [4]], 5]])).toStrictEqual([1, 2, 3, 4, 5])
+// })
 
-test('without', () => {
-  expect(without([1, 2, 3, 4, 5], [2, 4])).toStrictEqual([1, 3, 5])
-})
+// test('without', () => {
+//   expect(without([1, 2, 3, 4, 5], [2, 4])).toStrictEqual([1, 3, 5])
+// })
 
-test('combine', () => {
-  expect(combine([1, 2, 3], [2, 3, 4])).toStrictEqual([1, 2, 3, 2, 3, 4])
-  expect(combine([1, 2, 3], [2, 3, 4], [3, 4, 5])).toStrictEqual([1, 2, 3, 2, 3, 4, 3, 4, 5])
-})
+// test('combine', () => {
+//   expect(combine([1, 2, 3], [2, 3, 4])).toStrictEqual([1, 2, 3, 2, 3, 4])
+//   expect(combine([1, 2, 3], [2, 3, 4], [3, 4, 5])).toStrictEqual([1, 2, 3, 2, 3, 4, 3, 4, 5])
+// })
 
-test('combineUnique', () => {
-  expect(combineUnique([1, 2, 3], [2, 3, 4])).toStrictEqual([1, 2, 3, 4])
-  expect(combineUnique([1, 2, 3], [2, 3, 4], [3, 4, 5])).toStrictEqual([1, 2, 3, 4, 5])
-})
+// test('combineUnique', () => {
+//   expect(combineUnique([1, 2, 3], [2, 3, 4])).toStrictEqual([1, 2, 3, 4])
+//   expect(combineUnique([1, 2, 3], [2, 3, 4], [3, 4, 5])).toStrictEqual([1, 2, 3, 4, 5])
+// })
 
-test('combineWithout', () => {
-  expect(combineWithout('id', { id: 1, name: 'A' }, { id: 2, name: 'B' }, { id: 3, name: 'C' })).toStrictEqual([1, 2, 3])
-  expect(combineWithout('name', { id: 1, name: 'A' }, { id: 2, name: 'B' }, { id: 3, name: 'C' })).toStrictEqual(['A', 'B', 'C'])
-})
+// test('combineWithout', () => {
+//   expect(combineWithout('id', { id: 1, name: 'A' }, { id: 2, name: 'B' }, { id: 3, name: 'C' })).toStrictEqual([1, 2, 3])
+//   expect(combineWithout('name', { id: 1, name: 'A' }, { id: 2, name: 'B' }, { id: 3, name: 'C' })).toStrictEqual(['A', 'B', 'C'])
+// })
 
-test('reverse', () => {
-  expect(reverse([1, 2, 3])).toStrictEqual([3, 2, 1])
-})
+// test('reverse', () => {
+//   expect(reverse([1, 2, 3])).toStrictEqual([3, 2, 1])
+// })
 
-test('sortBy', () => {
-  const items = [
-    { name: 'John', age: 25 },
-    { name: 'Jane', age: 30 },
-    { name: 'Jake', age: 20 }
-  ]
-
-  expect(sortBy(items, 'age')).toStrictEqual([
-    { name: 'Jake', age: 20 },
-    { name: 'John', age: 25 },
-    { name: 'Jane', age: 30 }
-  ])
-
-  expect(sortBy(items, 'name')).toStrictEqual([
-    { name: 'Jake', age: 20 },
-    { name: 'Jane', age: 30 },
-    { name: 'John', age: 25 }
-  ])
-})
-
-test.skip('difference', () => {
-  expect(difference(['one', 'two', 'three'], ['one', 'two'])).toStrictEqual(['three'])
-  expect(difference([1, 2, 3], [2, 3, 4])).toStrictEqual([1, 4])
-  expect(difference([1, 2, 3], [2, 3, 4], [3, 4, 5])).toStrictEqual([1, 5])
-})
+// test.skip('difference', () => {
+//   expect(difference(['one', 'two', 'three'], ['one', 'two'])).toStrictEqual(['three'])
+//   expect(difference([1, 2, 3], [2, 3, 4])).toStrictEqual([1, 4])
+//   expect(difference([1, 2, 3], [2, 3, 4], [3, 4, 5])).toStrictEqual([1, 5])
+// })
