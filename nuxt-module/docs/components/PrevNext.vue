aecfa37a5b784963fef75f63e152f67279a4ca96
Jeremy Butler <jeremy@littlefox.studio>
2024-03-05 17:34:12 +1000

    refactor: update data references in template to use optional chaining for prev and next properties

--- nuxt-module/docs/components/PrevNext.vue
+++ nuxt-module/docs/components/PrevNext.vue
@@ -1,27 +1,30 @@
 <template>
-  <div class="mt-12 flex gap-4 text-balance" v-if="prev || next">
+  <div class="mt-12 flex gap-4 text-balance" v-if="data?.prev || data?.next">
     <NuxtLink
       prefetch
-      v-if="prev"
-      :to="prev._path"
+      v-if="data?.prev"
+      :to="data?.prev._path"
       class="flex flex-1 flex-col items-start gap-3 rounded-lg border p-5 text-left transition-all dark:border-white/10 dark:hover:bg-white/5">
       <Icon name="heroicons:arrow-left-circle" class="h-6 w-6 opacity-30" />
-      <div class="text-lg">{{ prev.title }}</div>
-      <div v-if="prev.lead" class="text-sm dark:text-white/50">{{ prev.lead }}</div>
+      <div class="text-lg">{{ data?.prev.title }}</div>
+      <div v-if="data?.prev.lead" class="text-sm dark:text-white/50">{{ data?.prev.lead }}</div>
     </NuxtLink>
     <NuxtLink
       prefetch
-      v-if="next"
-      :to="next._path"
+      v-if="data?.next"
+      :to="data?.next._path"
       class="flex flex-1 flex-col items-end gap-3 rounded-lg border p-5 text-right transition-all dark:border-white/10 dark:hover:bg-white/5">
       <Icon name="heroicons:arrow-right-circle" class="h-6 w-6 opacity-30" />
-      <div class="text-lg">{{ next.title }}</div>
-      <div v-if="next.lead" class="text-sm dark:text-white/50">{{ next.lead }}</div>
+      <div class="text-lg">{{ data?.next.title }}</div>
+      <div v-if="data?.next.lead" class="text-sm dark:text-white/50">{{ data?.next.lead }}</div>
     </NuxtLink>
   </div>
 </template>
 
 <script setup lang="ts">
   const route = useRoute()
-  const [prev, next] = await queryContent().only(['_path', 'title', 'lead']).findSurround(route.fullPath)
+  const { data } = await useAsyncData('next-prev' + route.fullPath, async () => {
+    const [prev, next] = await queryContent().only(['_path', 'title', 'lead']).findSurround(route.fullPath)
+    return { prev, next }
+  })
 </script>
