15e0bfafa7123c36c691573599e5088ab9a55d27
Jeremy Butler <jeremy@littlefox.studio>
2024-03-11 07:52:42 +1000

    style: refactor styling classes for consistency and readability

--- nuxt-module/docs/components/Example.vue
+++ nuxt-module/docs/components/Example.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="mt-6 flex flex-col divide-y overflow-clip rounded-xl border bg-indigo-50/40 shadow-indigo-300/[50%] transition-all focus-within:border-indigo-100 focus-within:shadow-2xl dark:divide-white/5 dark:border-white/[8%] dark:bg-white/[3%] dark:shadow-black/10">
+    class="mt-6 flex flex-col divide-y overflow-clip rounded-lg border bg-indigo-50/40 shadow-indigo-300/[50%] transition-all focus-within:border-indigo-100 focus-within:shadow-2xl dark:divide-white/5 dark:border-white/[8%] dark:bg-white/[3%] dark:shadow-black/10">
     <slot />
   </div>
 </template>
