e4e2f3f7260c7c088681976f4fc8369d0e0b7dcc
Jeremy Butler <jeremy@littlefox.studio>
2024-05-14 08:42:47 +1000

    Add animation functions and update data description

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,5 @@
 // title: Data
-// description: A collection of functions for formatting, filtering and arranging arrays and objects.
+// description: A collection of functions for formatting, filtering and taming wild arrays and objects.
 // lead: Arrays and objects, oh my!
 
 import { isObject, isArray } from './validators'
