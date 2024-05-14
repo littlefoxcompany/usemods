f80def9c29e48a91c44c4b30ca92e2ce3da62322
Phillip Hartin <phillip@littlefox.studio>
2024-05-14 17:27:57 +1000

    Add eslint config with stylistic and remove prettier

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+// eslint.config.js
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginVue from 'eslint-plugin-vue';
+import stylistic from '@stylistic/eslint-plugin'
+
+export default [
+  {
+    plugins: {
+      '@stylistic': stylistic
+    },
+    rules: {
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/quotes': ['error', 'single'],
+    }
+  },
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+]
