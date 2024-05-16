cd9e73c7984faaefac860cb9d89d6dd4545a9e55
Jeremy Butler <jeremy@littlefox.studio>
2024-05-16 19:15:17 +1000

    Add code examples and info to Vue components

--- nuxt-web/components/content/goodies/CheckPasswordStrength.vue
+++ nuxt-web/components/content/goodies/CheckPasswordStrength.vue
@@ -4,14 +4,14 @@
       <FormInput type="text" label="Password" v-model="value" />
     </ExampleInputs>
     <ExampleInputs>
-      <FormNumber label="Length" v-model="length" :min="1" />
-      <FormNumber label="Uppercase" v-model="uppercase" :min="0" />
-      <FormNumber label="Number" v-model="number" :min="0" />
-      <FormNumber label="Special" v-model="special" :min="0" />
+      <FormNumber label="Length" v-model="length" :min="1" info="Min 8" />
+      <FormNumber label="Uppercase" v-model="uppercase" :min="0" info="Min 1" />
+      <FormNumber label="Number" v-model="number" :min="0" info="Min 1" />
+      <FormNumber label="Special" v-model="special" :min="0" info="Min 1" />
     </ExampleInputs>
-
+    <ExampleCode :code="`checkPasswordStrength('${value}', { length: ${length}, uppercase: ${uppercase}, number: ${number}, special: ${special} })`" />
     <ExampleResult>
-      {{ checkPasswordStrength(value, length, uppercase, number, special) }}
+      {{ checkPasswordStrength(value, { length, uppercase, number, special }) }}
     </ExampleResult>
   </Example>
 </template>
--- nuxt-web/components/content/goodies/MergeFields.vue
+++ nuxt-web/components/content/goodies/MergeFields.vue
@@ -5,10 +5,11 @@
         <FormInput label="Value" type="text" v-model="value" />
       </ExampleInputs>
       <ExampleInputs>
-        <FormInput label="Name" v-model="name" placeholder="Enter name" />
-        <FormInput label="Vehicle" v-model="vehicle" placeholder="Enter vehicle" />
-        <FormInput label="Part" v-model="part" placeholder="Enter part" />
+        <FormInput label="Name" v-model="name" placeholder="Enter name" info="Example only"/>
+        <FormInput label="Vehicle" v-model="vehicle" placeholder="Enter vehicle" info="Example only"/>
+        <FormInput label="Part" v-model="part" placeholder="Enter part" info="Example only"/>
       </ExampleInputs>
+      <ExampleCode :code="`mergeFields(${value}, { name: ${name}, vehicle: ${vehicle}, part: ${part} })`" />
       <ExampleResult>
         {{ mergeFields(value, { name, vehicle, part }) }}
       </ExampleResult>
