8a198a3021433aac865602134d86860771464a87
Jeremy Butler <jeremy@littlefox.studio>
2024-05-25 08:57:25 +1000

    Refactor device detection and class assignment

--- src/devices.ts
+++ src/devices.ts
@@ -6,179 +6,158 @@
  * Check if you're a server-side user.
  */
 export function isServerSide(): boolean {
-    return typeof window === "undefined";
+  return typeof window === 'undefined'
 }
 
 /**
  * Adds device detection to your project's body class
  */
 export function addDeviceClasses(): void {
-    if (isServerSide()) return
-
-    const devices = new Map([
-        [isIos, 'ios'],
-        [isWindows, 'windows'],
-        [isLinux, 'linux'],
-        [isAndroid, 'android'],
-        [isMac, 'mac'],
-        [isChrome, 'chrome'],
-        [isFirefox, 'firefox'],
-        [isSafari, 'safari'],
-        [isEdge, 'edge'],
-        [isMobile, 'mobile'],
-        [isTablet, 'tablet'],
-        [isDesktop, 'desktop'],
-        [isPortrait, 'portrait'],
-        [isLandscape, 'landscape'],
-        [isBot, 'bot']
-    ]);
-
-    const classes = [];
-
-    for (const [check, className] of devices) {
-        if (check()) classes.push(className);
-    }
-
-    document.body.className = classes.join(' ');
-
-    // const updateOrientationClasses = () => {
-    //     document.body.classList.toggle('portrait', isPortrait());
-    //     document.body.classList.toggle('landscape', isLandscape());
-    // };
-
-    // let resizeTimeout: number | null = null;
-
-    // const debouncedUpdateOrientationClasses = () => {
-    //     if (resizeTimeout) { clearTimeout(resizeTimeout) }
-    //     resizeTimeout = window.setTimeout(updateOrientationClasses, 500);
-    // };
-
-    // window.addEventListener('resize', debouncedUpdateOrientationClasses);
-
-    // const removeEventListener = () => {
-    //     window.removeEventListener('resize', debouncedUpdateOrientationClasses);
-    // };
-    // removeEventListener();
+  if (isServerSide()) return
+  
+  const devices = new Map([
+    [isIos, 'ios'],
+    [isWindows, 'windows'],
+    [isLinux, 'linux'],
+    [isAndroid, 'android'],
+    [isMac, 'mac'],
+    [isChrome, 'chrome'],
+    [isFirefox, 'firefox'],
+    [isSafari, 'safari'],
+    [isEdge, 'edge'],
+    [isMobile, 'mobile'],
+    [isTablet, 'tablet'],
+    [isLandscape, 'landscape'],
+    [isBot, 'bot']
+  ])
+  
+  const classes = []
+  
+  for (const [check, className] of devices) {
+    if (check()) classes.push(className)
+  }
+  
+  document.body.className = classes.join(' ')
 }
 
 /**
  * Check if you're a passionate iPhone fan.
  */
 export function isIos(): boolean {
-    if (isServerSide()) return false;
-    return /iPad|iPhone|iPod/.test(navigator.platform);
+  if (isServerSide()) return false
+  return /iPad|iPhone|iPod/.test(navigator.platform)
 }
 
 /**
  * Check if you're a fervent Windows fan.
  */
 export function isWindows(): boolean {
-    if (isServerSide()) return false;
-    return /Win/.test(navigator.platform);
+  if (isServerSide()) return false
+  return /Win/.test(navigator.platform)
 }
 
 /**
  * Check if you're a devoted Linux fan.
  */
 export function isLinux(): boolean {
-    if (isServerSide()) return false;
-    return /Linux/.test(navigator.platform);
+  if (isServerSide()) return false
+  return /Linux/.test(navigator.platform)
 }
 
 /**
  * Check if you're a zealous Android fan.
  */
 export function isAndroid(): boolean {
-    if (isServerSide()) return false;
-    return /Android/.test(navigator.platform);
+  if (isServerSide()) return false
+  return /Android/.test(navigator.platform)
 }
 
 /**
  * Check if you're a staunch Mac fan.
  */
 export function isMac(): boolean {
-    if (isServerSide()) return false;
-    return /Mac/.test(navigator.platform);
+  if (isServerSide()) return false
+  return /Mac/.test(navigator.platform)
 }
 
 /**
  * Check if you're a die-hard Chrome fan.
  */
 export function isChrome(): boolean {
-    if (isServerSide()) return false;
-    return /Chrome/.test(navigator.userAgent);
+  if (isServerSide()) return false
+  return /Chrome/.test(navigator.userAgent)
 }
 
 /**
  * Check if you're a dedicated Firefox fan.
  */
 export function isFirefox(): boolean {
-    if (isServerSide()) return false;
-    return /Firefox/.test(navigator.userAgent);
+  if (isServerSide()) return false
+  return /Firefox/.test(navigator.userAgent)
 }
 
 /**
  * Check if you're a lonely Safari fan.
  */
 export function isSafari(): boolean {
-    if (isServerSide()) return false;
-    return /Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent);
+  if (isServerSide()) return false
+  return /Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent)
 }
 
 /**
  * Check if you're an ardent Edge fan.
  */
 export function isEdge(): boolean {
-    if (isServerSide()) return false;
-    return /Edge/.test(navigator.userAgent);
+  if (isServerSide()) return false
+  return /Edge/.test(navigator.userAgent)
 }
 
 /**
  * Check if you're rocking a mobile
  */
 export function isMobile(): boolean {
-    if (isServerSide()) return false;
-    return /Mobi/.test(navigator.userAgent);
+  if (isServerSide()) return false
+  return /Mobi/.test(navigator.userAgent)
 }
 
 /**
  * Check if you're tablet user
  */
 export function isTablet(): boolean {
-    if (isServerSide()) return false;
-    return /Tablet/.test(navigator.userAgent);
+  if (isServerSide()) return false
+  return /Tablet/.test(navigator.userAgent)
 }
 
 /**
  * Check if you're pro desktop user
  */
 export function isDesktop(): boolean {
-    if (isServerSide()) return false;
-    return !isMobile() && !isTablet();
+  if (isServerSide()) return false
+  return !isMobile() && !isTablet()
 }
 
 /**
  * Check if you're portrait
  */
 export function isPortrait(): boolean {
-    if (isServerSide()) return false;
-    return window.innerHeight > window.innerWidth;
+  if (isServerSide()) return false
+  return window.innerHeight > window.innerWidth
 }
 
 /**
  * Check if you're landscape
  */
 export function isLandscape(): boolean {
-    if (isServerSide()) return false;
-    return window.innerWidth > window.innerHeight;
+  if (isServerSide()) return false
+  return window.innerWidth > window.innerHeight
 }
 
 /**
  * Check if you're a cyborg or a bot
  */
 export function isBot(): boolean {
-    if (isServerSide()) return false;
-    return /bot|googlebot|crawler|spider|robot|crawling/i.test(
-        navigator.userAgent
-    );
+  if (isServerSide()) return false
+  return /bot|googlebot|crawler|spider|robot|crawling/i.test(
+    navigator.userAgent
+  )
 }
