3d4ac57f17568585011b3c7e788803c3e4fe6eb7
Jeremy Butler <jeremy@littlefox.studio>
2024-05-25 20:07:34 +1000

    Refactor CSS styles and update components***

--- nuxt-web/components/content/PageFunction.vue
+++ nuxt-web/components/content/PageFunction.vue
@@ -1,14 +1,18 @@
 <template>
   <section class="pb-12 text-gray-900 dark:text-white" :id="name" ref="section">
 
-    <div @click="copyToClipboard(`${detectHost()}#${props.name.toLowerCase()}`), copied()" class="relative flex w-fit cursor-pointer items-center gap-3" @mouseover="showCopyToClipboard = true" @mouseout="showCopyToClipboard = false">
+    <div 
+      class="relative flex w-full cursor-pointer items-end gap-3"
+      @click="copyToClipboard(`${detectHost()}#${props.name.toLowerCase()}`), copied()"
+      @mouseover="showCopyToClipboard = true"
+      @mouseout="showCopyToClipboard = false">
       <h2 class="text-3xl font-semibold">{{ name }}</h2>
 
-      <div class="flex h-6 w-6 items-center justify-center rounded-md border border-white/5 bg-white/[3%] text-zinc-500 transition-all hover:border-white/20 hover:text-white" :class="showCopyToClipboard ? 'translate-y-0 opacity-100' : 'translate-y-2 opacity-0'">
+      <div class="flex h-6 w-6 mb-1 items-center justify-center rounded-md border border-white/5 bg-white/[3%] text-zinc-500 transition-all hover:border-white/20 hover:text-white" :class="showCopyToClipboard ? 'translate-y-0 opacity-100' : 'translate-y-2 opacity-0'">
         <Icon name="heroicons:hashtag" class="h-4 w-4" />
       </div>
 
-      <div class="absolute right-0 mt-1 flex h-6 items-center justify-center rounded-md border border-white/5 bg-white/[3%] px-1.5 text-zinc-500 transition-all" :class="showCopied ? 'translate-y-0 opacity-100' : 'translate-y-2 opacity-0'">
+      <div class="absolute right-0 mt-1 flex h-6 buttom-4 items-center justify-center rounded-md border border-white/5 bg-white/[3%] px-1.5 text-zinc-500 transition-all" :class="showCopied ? 'translate-y-0 opacity-100' : 'translate-y-2 opacity-0'">
         Link Copied!
       </div>
     </div>
