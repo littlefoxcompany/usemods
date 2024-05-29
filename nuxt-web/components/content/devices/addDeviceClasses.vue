ef89c6e590ca9282774e4667c181d5854e67591e
Jeremy Butler <jeremy@littlefox.studio>
2024-05-30 09:37:58 +1000

    chore: Update npm dependencies and dev tools versions

--- nuxt-web/components/content/devices/addDeviceClasses.vue
+++ nuxt-web/components/content/devices/addDeviceClasses.vue
@@ -1,12 +1,15 @@
 <template>
     <Example>
         <ExampleResult>
-        {{ fetchDeviceClasses() }}
+          <ClientOnly>
+            {{ fetchDeviceClasses() }}
+          </ClientOnly>
         </ExampleResult>
     </Example>
 </template>
 
 <script setup lang="ts">
+
 function fetchDeviceClasses() {
   const body = document.querySelector('body')
   const classes = body?.classList.value
