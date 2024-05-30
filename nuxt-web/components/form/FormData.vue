455c1d9133c8fd9cce9db0920782f67e511d2f48
Jeremy Butler <jeremy@littlefox.studio>
2024-05-30 20:14:58 +1000

    For json data formating

--- nuxt-web/components/form/FormData.vue
+++ nuxt-web/components/form/FormData.vue
@@ -1,23 +1,9 @@
 <template>
-  <pre class="w-full rounded-lg border border-white/5 bg-white/[3%] p-4 font-mono text-sm" v-html="styledJson"></pre>
+  <pre class="w-full rounded-lg border border-white/5 bg-white/[3%] p-4 font-mono text-sm">{{ props.data }}</pre>
 </template>
 
 <script setup lang="ts">
-  const props = defineProps({
-    data: Object || Array
-  })
-
-  const styledJson = computed(() => {
-    const jsonString = JSON.stringify(props.data, null, 2)
-    const styledString = jsonString.replace(/(["\{\}\[\]\,])/g, (match) => {
-      return `<span class="json-special">${match}</span>`
-    })
-    return styledString
-  })
+const props = defineProps({
+  data: Object || Array
+})
 </script>
-
-<style>
-  .json-special {
-    @apply font-thin text-indigo-500 dark:text-white/60;
-  }
-</style>
