63b3718585ce66bd86d54e6035e246757da0a44f
Jeremy Butler <jeremy@littlefox.studio>
2024-07-13 10:20:47 +1000

    chore: Update TableOfContents.vue to include item titles in generated markdown

--- nuxt-web/components/TableOfContents.vue
+++ nuxt-web/components/TableOfContents.vue
@@ -1,17 +1,18 @@
 <template>
   <nav class="text-gray-900 dark:text-white">
     <p class="pb-4 font-semibold">On this page</p>
+    
     <ul>
-      <li v-for="(link, index) in data?.toc" :key="index" class="mt-0">
+      <li v-for="item in data" :key="item.id" class="mt-0">
         <NuxtLink
-          v-if="link"
+          v-if="item.id"
           class="flex cursor-pointer gap-3 text-sm font-medium leading-none text-gray-500 hover:text-indigo-600 dark:hover:text-white/75"
-          @click="scrollToAnchor(link)">
+          @click="scrollToAnchor(item.id)">
           <div
             class="w-2 border-l transition-all"
-            :class="activeSections.includes(link) ? ' border-indigo-600 dark:border-amber-400 ' : 'border-gray-200 dark:border-white/15'"></div>
-          <div class="truncate py-1.5" :class="activeSections.includes(link) ? 'text-indigo-600 dark:text-amber-400' : 'text-gray-500 dark:text-white/50'">
-            {{ link }}
+            :class="activeSections.includes(item.id) ? ' border-indigo-600 dark:border-amber-400 ' : 'border-gray-200 dark:border-white/15'"></div>
+          <div class="truncate py-1.5" :class="activeSections.includes(item.id) ? 'text-indigo-600 dark:text-amber-400' : 'text-gray-500 dark:text-white/50'">
+            {{ item.title }}
           </div>
         </NuxtLink>
 
@@ -33,11 +34,14 @@ const route = useRoute()
 const activeSections = useState('activeSections', () => [])
 const pageId = useRoute().params.slug
 
-const { data } = useAsyncData('toc' + pageId, () => queryContent(route.path).findOne().then(
+const { data } = useAsyncData('toc' + pageId, () => queryContent(route.path).only('body').findOne().then(
+  toc => toc.body?.children
+    .map(link => ({ id: link.props?.name ?? link.props?.id, title: link.children?.[0]?.value ?? link.props?.name ?? link.props?.id }))
+    .filter(link => link.id && link.title)
 ))
 
 // Function to scroll to the anchor
-function scrollToAnchor(id) {
+function scrollToAnchor(id: string) {
   const element = document.getElementById(id)
   if (element) {
     element.scrollIntoView({ behavior: 'smooth' })
