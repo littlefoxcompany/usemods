614f8f54a9381abb452d507983496f003d2eaeb0
Jeremy Butler <jeremy@littlefox.studio>
2024-10-06 13:41:45 +1000

    chore: Update CSS classes for Button and Example components

--- nuxt-web/components/Example.vue
+++ nuxt-web/components/Example.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="mt-6 flex flex-col divide-y text-clip rounded-xl border bg-indigo-50/40 shadow-indigo-300/[50%] transition-all dark:divide-white/5 dark:border-white/[8%] dark:bg-white/[3%] dark:shadow-black/10">
+    class="mt-6 flex flex-col divide-y overflow-hidden rounded-xl border bg-indigo-50/40 shadow-indigo-300/[50%] transition-all dark:divide-white/5 dark:border-white/[8%] dark:bg-white/[3%] dark:shadow-black/10">
     <slot />
   </div>
 </template>
