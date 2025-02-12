7cc2220773c57fb120dc026f285c0f8b01bd29b2
Jeremy Butler <jeremy@jeremymbutler.com>
2025-02-13 08:16:12 +1000

    feat(generators): add components for encoding and decoding Short UUIDs; update Unix timestamp handling

---
+++ nuxt-web/components/content/generators/decodeShortUuid.vue
@@ -0,0 +1,15 @@
+<template>  
+    <Example>
+    <ExampleInputs>
+      <FormInput v-model="shortUuid" label="Short UUID" />
+    </ExampleInputs>
+    <ExampleCode :code="`decodeShortUuid(${shortUuid})`" />
+    <ExampleResult>
+      {{ decodeShortUuid(shortUuid) }}
+    </ExampleResult>
+  </Example>
+</template>
+
+<script setup lang="ts">
+const shortUuid = ref('Lt7bEcatSvm81jCJajLItA')
+</script>
---
+++ nuxt-web/components/content/generators/encodeShortUuid.vue
@@ -0,0 +1,15 @@
+<template>
+  <Example>
+    <ExampleInputs>
+      <FormInput v-model="uuid" label="UUID" />
+    </ExampleInputs>
+    <ExampleCode :code="`encodeShortUuid(${uuid})`" />
+    <ExampleResult>
+      {{ encodeShortUuid(uuid) }}
+    </ExampleResult>
+  </Example>
+</template>
+
+<script setup lang="ts">
+const uuid = ref('2ededb11-c6ad-4af9-bcd6-30896a32c8b4')
+</script>
