3477da67cd3a64fa50775bf14e656193f16253cd
Jeremy Butler <jeremy@jeremymbutler.com>
2025-02-15 07:01:01 +1000

    feat(generators): add result state and generate button to number generation components

--- nuxt-web/components/content/generators/GenerateNumber.client.vue
+++ nuxt-web/components/content/generators/GenerateNumber.client.vue
@@ -1,18 +1,22 @@
 <template>
   <Example>
-    <ExampleInputs>
+    <ExampleInputs class="flex gap-2">
       <FormNumber
         v-model="length"
         label="Length"
         :min="1" />
+      <Button color="secondary" @click="result = generateNumber(length)">
+        Generate
+      </Button>
     </ExampleInputs>
     <ExampleCode :code="`generateNumber(${length})`" />
     <ExampleResult>
-      {{ generateNumber(length) }}
+      {{ result }}
     </ExampleResult>
   </Example>
 </template>
 
 <script setup lang="ts">
 const length = ref(8)
+const result = ref(0)
 </script>
